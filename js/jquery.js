ef3bac0b9d31a00f16d07d639dce94717772e20d
AlfredoFilho <alfredoalbelisb@gmail.com>
2020-05-05 15:37:29 -0300

    get values | box confirmation | loading overlay

--- js/jquery.js
+++ js/jquery.js
@@ -5,6 +5,45 @@ $(document).ready(function(){
     });
     
     $("#btnCorrigir").click(function(){
-        alert('Em construção!')
+
+        respostasAlt = ""
+        for (i = 1; i <= 30; ++i) {
+            
+            position = String(i)
+
+            if( i < 10){
+                position = "0" + String(i)
+            }
+
+            respostasAlt = respostasAlt + position + " = " + $("#alt" + position).val() + "<br>"
+        }
+
+        for (i = 31; i <= 40; ++i) {
+            
+            position = String(i)
+
+            if( i < 10){
+                position = "0" + String(i)
+            }
+
+            respostasAlt = respostasAlt + position + " = " + $("#out" + position).val() + "<br>"
+        }
+
+        $.MessageBox({
+            buttonDone  : "OK",
+            buttonFail  : "Voltar",
+            message     : respostasAlt
+        }).done(function(){
+            $.LoadingOverlay("show");
+
+            setTimeout(function(){
+                $.LoadingOverlay("hide");
+                $.MessageBox("Em construção!");
+            }, 2000);
+
+        }).fail(function(){
+        
+        });
+
     });
 });
