b1420b6c54ada588a4f029549c9f80902a33a0f6
AlfredoFilho <alfredoalbelisb@gmail.com>
2020-05-05 13:55:37 -0300

    add btnLimpar

---
+++ js/jquery.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    $("#btnLimpar").click(function(){
+        $(':input[type=text]').val('')
+    });
+    
+    $("#btnCorrigir").click(function(){
+        alert('Em construção!')
+    });
+});
