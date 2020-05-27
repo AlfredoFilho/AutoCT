049724a7b8f2c392e7a17016ee929685e9e93524
AlfredoFilho <alfredoalbelisb@gmail.com>
2020-05-26 21:24:28 -0300

    confirmation box

--- js/jquery.js
+++ js/jquery.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
 
-    var file = ''
-    var form;
+    var fileInB64 = ''
     
     function getBase64(file) {
         var reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = function () {
             fileInB64 = reader.result.split(',')[1];
-            console.log(fileInB64)
         };
         reader.onerror = function (error) {
           console.log('Error: ', error);
@@ -21,11 +19,8 @@ $(document).ready(function(){
 
     $("#file").change(function() {
         $(".divFile").css({"background-color":"#8080800d"});
-        // var file = document.querySelector('.divFile > input[type="file"]').files[0];
-        // console.log(file.target.result)
-        // getBase64(file)
-        form = new FormData();
-        form.append('file', event.target.files[0]);
+        var file = document.querySelector('.divFile > input[type="file"]').files[0];
+        getBase64(file)
     });
     
     function checkFile(){
@@ -53,24 +48,53 @@ $(document).ready(function(){
     }
 
     function uploadFile(){
-        $.ajax({
+        return $.ajax({
             url: 'https://r0oq6xy9te.execute-api.us-east-2.amazonaws.com/AutoCT-API/upload',
             crossDomain : true,
             processData: false,
-            data: form,
+            data: fileInB64,
             contentType: 'image/png',
             type: 'POST',
             success: function (data) {
-                console.log(data)
+                return data
             }
         });
     }
 
-    $("#btnCorrigir").click(function(){
+    function setDataDiv(data){
+
+        console.log(data['body']['alternativas'])
+        $('#respostasAPI').text(JSON.stringify(data['body']['alternativas'], null, 2));
+
+        $(window).scrollTop($('#respostasAPI').offset().top);
+
+    }
+
+    function confirmBox(){
+
+        $.MessageBox({
+            buttonDone  : "OK",
+            buttonFail  : "Voltar",
+            message     : '<strong>Confirmar respostas</strong><br><br>XX: xx'
+        }).done(async function(){
+            $.LoadingOverlay("show");
+
+            // var data = await uploadFile()
+            // setDataDiv(data)
+            alert("Em construção!")
+            
+            $.LoadingOverlay("hide");
+
+        }).fail(function(){
+
+        });
+
+    }
+
+    $("#btnCorrigir").click(async function(){
         if (checkFile() == true){
             if (checkInputs() == true){
-                // uploadFile()
-                alert("Em construção!")
+                confirmBox()
             } else{
                 alert("Você deixou algum input vazio!")
             }
@@ -78,6 +102,6 @@ $(document).ready(function(){
         } else{
             alert("Você não selecionou um arquivo!")
         }
-        // uploadFile()
     });
+
 });
